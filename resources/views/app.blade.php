d3e3680f761b0391ec3033e9d182fae3b31e07a8
Noman Ur Rehman <nomanurrehman@gmail.com>
2015-02-23 18:53:03 +0500

    Update authentication routes and views

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,7 +46,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->first_name . ' ' . Auth::user()->last_name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logout</a></li>
+								<li><a href="{{ action('Auth\AuthController@getLogout') }}">Logout</a></li>
 							</ul>
 						</li>
 					@endif
